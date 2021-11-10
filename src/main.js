747bdc1c284007f1da8f0cda286358347be27325
Maurizio-P <mauriziopignotti@gmail.com>
2021-11-10 19:35:44 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
